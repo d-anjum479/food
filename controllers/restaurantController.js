6f89481af8ec97bac7738ec443e318730d94fa2e
Daniyal Anjum <d.anjum479@gmail.com>
2024-03-12 10:24:43 +0500

    get all restaurants, single restaurant by id delete restaurant by id, all done in restaurant api except update restaurant values

--- controllers/restaurantController.js
+++ controllers/restaurantController.js
@@ -49,5 +49,103 @@ const createRestaurantController = async (req, res) => {
         .send({ success: false, message: "Error in restaurant api", error });
   }
 };
+const getAllRestaurantController = async (req, res) => {
+  try {
+    const restaurants = await Restaurant.find({});
+    if (!restaurants) {
+      res.status(500).send({
+        success: false,
+        message: "No restaurants found",
+        error,
+      });
+    }
 
-export { createRestaurantController };
+    res.status(200).send({
+      success: true,
+      totalCount: restaurants.length,
+      restaurants,
+    });
+  } catch (error) {
+    console.log(error),
+      res.status(500).send({
+        success: false,
+        message: "Error in getAllRestaurantController api",
+        error,
+      });
+  }
+};
+const getRestaurantByIdController = async (req, res) => {
+  try {
+    const restaurantId = req.params.id;
+    if (!restaurantId) {
+      res.status(404).send({
+        success: false,
+        message: "Please provide restaurant id",
+        error,
+      });
+    }
+
+    const restaurant = await Restaurant.findById({ _id: req.params.id });
+    if (!restaurant) {
+      res.status(500).send({
+        success: false,
+        message: "No restaurant found",
+        error,
+      });
+    }
+
+    res.status(200).send({
+      success: true,
+      restaurant,
+    });
+  } catch (error) {
+    console.log(error),
+      res.status(500).send({
+        success: false,
+        message: "Error in getRestaurantByIdController api",
+        error,
+      });
+  }
+};
+const deleteRestaurantController = async (req, res) => {
+  try {
+    const restaurantId = req.params.id;
+    if (!restaurantId) {
+      res.status(404).send({
+        success: false,
+        message: "Please provide restaurant id",
+        error,
+      });
+    }
+
+    const restaurant = await Restaurant.findByIdAndDelete({
+      _id: req.params.id,
+    });
+    if (!restaurant) {
+      res.status(500).send({
+        success: false,
+        message: "No restaurant found",
+        error,
+      });
+    }
+
+    res.status(200).send({
+      success: true,
+      message: "Restaurant deleted successfully",
+    });
+  } catch (error) {
+    console.log(error),
+      res.status(500).send({
+        success: false,
+        message: "Error in deleteRestaurantController api",
+        error,
+      });
+  }
+};
+
+export {
+  createRestaurantController,
+  getAllRestaurantController,
+  getRestaurantByIdController,
+  deleteRestaurantController,
+};
--- routes/restaurantRoutes.js
+++ routes/restaurantRoutes.js
@@ -1,11 +1,23 @@
 import express from "express";
 
 import { authMiddleware } from "../middlewares/authMiddleware.js";
-import { createRestaurantController } from "../controllers/restaurantController.js";
+import {
+  createRestaurantController,
+  deleteRestaurantController,
+  getAllRestaurantController,
+  getRestaurantByIdController,
+} from "../controllers/restaurantController.js";
 
 const router = express.Router();
 
 // Create Restaurant || POST
 router.post("/create", authMiddleware, createRestaurantController);
+
+// Get all restaurant to display on frontend
+router.get("/all-restaurants", getAllRestaurantController);
+// Get single restaurant
+router.get("/get-restaurant/:id", getRestaurantByIdController);
+// Delete Restaurant
+router.delete("/delete/:id", authMiddleware, deleteRestaurantController);
 
 export default router;
